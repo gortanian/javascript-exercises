361033ce843957ab2d4360d77a77c72e51fd746b
gortanian <gortanian@gmail.com>
2022-08-01 13:15:23 -0700

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,33 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // initialize isLeapYear
+    let isLeapYear = false;
 
+    // write a function that determines if a year is divisible by another number
+    function isDivisibleBy(year, number) {
+        if (year % number === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    if (isDivisibleBy(year, 400)) {
+        isLeapYear = true;
+    }
+
+    else if (isDivisibleBy(year, 100)) {
+        isLeapYear = false;
+    }
+
+    else if (isDivisibleBy(year, 4)) {
+        isLeapYear = true;
+    }
+    else {
+        isLeapYear = false; 
+    }
+    
+    return isLeapYear;
 };
 
 // Do not edit below this line
