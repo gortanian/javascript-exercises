4615ca761c4f6534b6c7e12162f193520471cf8a
gortanian <gortanian@gmail.com>
2022-08-01 12:56:04 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    // returns the sum of all numbers in-between and including int1 and int2
 
+    // check if int1 and int2 are actually numbers
+    if (typeof int1 != "number" || typeof int2 != "number") {
+        return "ERROR";
+    }
+
+    // check if int1 or int2 are negative
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+
+    // check if int1 is greater than int2, if so, swap them
+    if (int1 > int2) {
+        let temp = int1; 
+        int1 = int2;
+        int2 = temp;
+    }
+
+    // initialize a variable called sum
+    let sum = 0;
+
+    // for numbers between int1 and int2, inclusive, add them to the sum. 
+    for (i = int1; i <= int2; i++) {
+        sum += i;
+    }
+
+    // return the sum 
+    return sum;
 };
 
 // Do not edit below this line
